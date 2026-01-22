2d82ef0d8e5630bdc5b21d91cb540d518cfe22da
Kevin Su <kevin.su@ufl.edu>
2026-01-22 17:48:48 -0500

    Completed binarize.sv

--- rtl/binarize.sv
+++ rtl/binarize.sv
@@ -1,23 +1,32 @@
-
-// Currently combinational
+// Currently sequential for pipeline
 module binarize #(
     parameter int PIXEL_NUM         = 784,
     parameter int PIXEL_DATA_WIDTH  = 8,
     parameter int OUTPUT_DATA_WIDTH = 784
 ) (
-    input  logic                 clk,
-    input  logic                 rst,
-    input  logic [PIXEL_NUM-1:0] IN_DATA [PIXEL_DATA_WIDTH-1:0],
-    input  logic                 EN,
-    output logic                 OUT_DATA [OUTPUT_DATA_WIDTH-1:0]
-    
+    input  logic                          clk,
+    input  logic                          rst,
+    input  logic                          EN,
+    input  logic [PIXEL_DATA_WIDTH-1:0]   IN_DATA [PIXEL_NUM-1:0],
+    output logic [OUTPUT_DATA_WIDTH-1:0]  OUT_DATA
 );
 
-    always_ff @(posedge(clk) | posedge(rst)) begin
-        OUT_DATA = OUT_DATA;
-        if(EN) begin
-        
+    always_ff @(posedge(clk) or posedge(rst)) begin
+        if(rst) begin
+            OUT_DATA <= '0;
         end
-    end
+        else
+            // OUT_DATA <= '0;
+            if(EN) begin
+                for(int i=0; i<PIXEL_NUM; i++) begin
+                    if(IN_DATA[i] >= 8'h80) begin
+                        OUT_DATA[i] <= 1'b1;
+                    end
+                    else begin
+                        OUT_DATA[i] <= 1'b0;
+                    end
+                end
+            end
+        end
 
 endmodule
