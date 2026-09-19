839d4cfb9779908cb533b9a96aa960cef8f152eb
kevinsu-rgb <kevin.su@ufl.edu>
2026-09-19 16:12:32 -0700

    remove popcount.sv and update binarize.sv to use a new implementation

--- rtl/binarize.sv
+++ rtl/binarize.sv
@@ -5,9 +5,9 @@ module binarize #(
 ) (
     input  logic                          clk,
     input  logic                          rst,
-    input  logic                          EN,
-    input  logic [PIXEL_DATA_WIDTH-1:0]   IN_DATA [PIXEL_NUM-1:0],
-    output logic [OUTPUT_DATA_WIDTH-1:0]  OUT_DATA
+    input  logic                          en,
+    input  logic [PIXEL_DATA_WIDTH-1:0]   in_data [PIXEL_NUM-1:0],
+    output logic [OUTPUT_DATA_WIDTH-1:0]  out_data
 );
 
     logic [OUTPUT_DATA_WIDTH-1:0] binarized_data;
@@ -22,15 +22,15 @@ module binarize #(
     always_comb begin
         binarized_data = '0;
         for (int i = 0; (i < PIXEL_NUM) && (i < OUTPUT_DATA_WIDTH); i++) begin
-            binarized_data[i] = (IN_DATA[i] >= BINARIZE_THRESHOLD);
+            binarized_data[i] = (in_data[i] >= BINARIZE_THRESHOLD);
         end
     end
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            OUT_DATA <= '0;
-        end else if (EN) begin
-            OUT_DATA <= binarized_data;
+            out_data <= '0;
+        end else if (en) begin
+            out_data <= binarized_data;
         end
     end
 
